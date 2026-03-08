dba90960aa254364a8ba53d0aa193ab0a522d4dd
Soroka Ilya <ilya010s@mail.ru>
2026-03-08 22:55:32 +1000

    small fix

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, IceCream, Topping, Wrapper
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    empty_value_display = 'Не задано'
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+
+
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+admin.site.register(Category)
